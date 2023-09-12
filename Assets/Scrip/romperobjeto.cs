00795acdbab64aae6a8098f169e33fdcfcd8457d
CrimroseRojas <paularos2020@gmail.com>
2023-09-12 16:36:21 -0500

    cree el scrip para romper objetos

---
+++ Assets/Scrip/romperobjeto.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class romperobjeto : MonoBehaviour
+{ 
+    public GameObject romperObjeto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

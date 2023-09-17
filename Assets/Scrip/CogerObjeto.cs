4ff25f34291835c8999780e90f596473b13bd5b4
artherx <xxartherxx@gmail.com>
2023-09-17 18:38:11 -0500

    se añadio un time para agarrar y se elimino que se buscara la ubicacion pa las manos

--- Assets/Scrip/CogerObjeto.cs
+++ Assets/Scrip/CogerObjeto.cs
@@ -10,17 +10,17 @@ public class CogerObjeto : MonoBehaviour
     public GameObject manoD;
     public float fuerzaLanza = 1000f;
     private GameObject pickObjetc = null;
+    public GameObject personaje = null;
     private Vector3 mIPos,mIRot;
     private Vector3 mDPos,mDRot;
-
+    
+    private float t1,t2;
+    private bool t3 = false;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyUp("e")&&pickObjetc!=null && Input.GetKey("f")!=true)
         {
-           
-            manoD.transform.position = mIPos + manos.transform.position;
-            manoI.transform.position = mDPos + manos.transform.position;
             pickObjetc.GetComponent<Rigidbody>().useGravity = true;
             pickObjetc.GetComponent<Rigidbody>().isKinematic = false;
             pickObjetc.gameObject.transform.SetParent(null);
@@ -28,15 +28,14 @@ public class CogerObjeto : MonoBehaviour
             Debug.Log("soltar objeto");
             
         }
-        if(Input.GetKey("f")&&pickObjetc!=null&& Input.GetKey("e") != true)
+        if(Input.GetKey("f")&&pickObjetc!=null)
         {
-            
-            manoD.transform.rotation = manos.transform.rotation;
-            manoI.transform.rotation = manos.transform.rotation;
+            t1= Time.time;
+            t3=true;
             pickObjetc.GetComponent<Rigidbody>().useGravity = true;
             pickObjetc.GetComponent<Rigidbody>().isKinematic = false;
             pickObjetc.gameObject.transform.SetParent(null);
-            pickObjetc.GetComponent<Rigidbody>().AddForce((transform.forward+new Vector3(0,2,0))*fuerzaLanza);
+            pickObjetc.GetComponent<Rigidbody>().AddForce((personaje.transform.forward+new Vector3(0,2,0))*fuerzaLanza);
             
             pickObjetc = null;
             Debug.Log("lanzar objeto"+ transform.forward+"con fuerza:"+ transform.forward*fuerzaLanza);
@@ -47,20 +46,18 @@ public class CogerObjeto : MonoBehaviour
     private void OnTriggerStay(Collider other) {
         if(other.gameObject.CompareTag("objeto"))
         {
+            t2= Time.time;
+            if(t2-t1>=0.50&& t3==true) t3 = false;
             Debug.Log("objeto en rango");
-            if (Input.GetKey("e")&&pickObjetc==null)
+            if (Input.GetKey("e")&&pickObjetc==null&&t3==false)
             {
-                Debug.Log("woaos");
+                Debug.Log(t2-t1);
                 other.GetComponent<Rigidbody>().useGravity = false;
                 other.GetComponent<Rigidbody>().isKinematic = true;
                 other.transform.position = manos.transform.position;
                 other.gameObject.transform.SetParent(manos.gameObject.transform);
                 pickObjetc = other.gameObject;
-                mIPos = manoI.transform.position - manos.transform.position;
-                mDPos = manoD.transform.position - manos.transform.position;
-                //Mover manos al objetos
-                manoD.transform.position = other.transform.position;
-                manoI.transform.position = other.transform.position;
+                
                 Debug.Log("agarrar objeto");
             }
         }

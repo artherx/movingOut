eeb5a4f9ea7abce79bd5f49b5a6e05ad3204d67a
ErwinPardo <143192196+ErwinPardo@users.noreply.github.com>
2023-09-17 17:11:54 -0500

    cositas yo que se

--- Assets/Scrip/CogerObjeto.cs
+++ Assets/Scrip/CogerObjeto.cs
@@ -10,22 +10,31 @@ public class CogerObjeto : MonoBehaviour
     public GameObject manoD;
     public float fuerzaLanza = 1000f;
     private GameObject pickObjetc = null;
-    private Vector3 mIVector;
-    private Vector3 mDVector;
+    private Vector3 mIPos,mIRot;
+    private Vector3 mDPos,mDRot;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp("e")&&pickObjetc!=null)
+        if(Input.GetKeyUp("e")&&pickObjetc!=null && Input.GetKey("f")!=true)
         {
+            manoD.transform.rotation = manos.transform.rotation;
+            manoI.transform.rotation = manos.transform.rotation;
+            manoD.transform.position = mIPos + manos.transform.position;
+            manoI.transform.position = mDPos + manos.transform.position;
             pickObjetc.GetComponent<Rigidbody>().useGravity = true;
             pickObjetc.GetComponent<Rigidbody>().isKinematic = false;
             pickObjetc.gameObject.transform.SetParent(null);
             pickObjetc = null;
             Debug.Log("soltar objeto");
+            
         }
-        if(Input.GetKey("f")&&pickObjetc!=null)
+        if(Input.GetKey("f")&&pickObjetc!=null&& Input.GetKey("e") != true)
         {
+            manoD.transform.position = mIPos + manos.transform.position;
+            manoI.transform.position = mDPos + manos.transform.position;
+            manoD.transform.rotation = manos.transform.rotation;
+            manoI.transform.rotation = manos.transform.rotation;
             pickObjetc.GetComponent<Rigidbody>().useGravity = true;
             pickObjetc.GetComponent<Rigidbody>().isKinematic = false;
             pickObjetc.gameObject.transform.SetParent(null);
@@ -33,20 +42,24 @@ public class CogerObjeto : MonoBehaviour
             
             pickObjetc = null;
             Debug.Log("lanzar objeto"+ transform.forward+"con fuerza:"+ transform.forward*fuerzaLanza);
+            
+
         }
     }
     private void OnTriggerStay(Collider other) {
         if(other.gameObject.CompareTag("objeto"))
         {
-            if (Input.GetKeyDown("e")&&pickObjetc==null)
+            Debug.Log("objeto en rango");
+            if (Input.GetKey("e")&&pickObjetc==null)
             {
+                Debug.Log("woaos");
                 other.GetComponent<Rigidbody>().useGravity = false;
                 other.GetComponent<Rigidbody>().isKinematic = true;
                 other.transform.position = manos.transform.position;
                 other.gameObject.transform.SetParent(manos.gameObject.transform);
                 pickObjetc = other.gameObject;
-                mIVector = manoI.transform.position;
-                mDVector = manoD.transform.position;
+                mIPos = manoI.transform.position - manos.transform.position;
+                mDPos = manoD.transform.position - manos.transform.position;
                 //Mover manos al objetos
                 manoD.transform.position = other.transform.position;
                 manoI.transform.position = other.transform.position;
